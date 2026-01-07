68eee2fa35d2d3689f0b00bacb97cca672b99da5
AdvancingHermit <christiannkya@outlook.com>
2026-01-07 23:13:13 +0100

    Tror basically det det her vi skal gøre

--- src/Main.java
+++ src/Main.java
@@ -5,126 +5,88 @@ import java.util.List;
 import java.util.Scanner;
 
 
-class Index1 {
+class Main {
 
-    WikiItem start;
-    private static String END = "---END.OF.DOCUMENT---";
+    private static boolean PrintDocs = false;
 
-    private class WikiDoc {
-        String header;
-        WikiDoc next;
-
-        WikiDoc(String header, WikiDoc next){
-            this.header = header;
-            this.next = next;
-        }
+    public static void SearchIndex1(String str, Index1 index1) {
+        var res = index1.search(str);
+        System.out.println("Index1 Result: " + res);
+    }
+    public static void PreProcessAndSearchIndex1(String filepath, String str) {
+        Index1 i1 = new Index1(filepath);
+        SearchIndex1(str, i1);
     }
 
-    private class WikiItem {
-        String str;
-        WikiItem next;
-        WikiDoc docsIn;
+    public static void SearchIndex2(String str, Index2 index2) {
+        var res = index2.search(str);
+        System.out.println("Index2 Result: " + res);
 
-        WikiItem(String s, WikiItem n) {
-            str = s;
-            next = n;
-            docsIn = null;
-        }
-
-        public Boolean Contains(WikiDoc doc){
-            WikiDoc currDoc = docsIn;
-            while (currDoc != null){
-                if (currDoc == doc) return true;
-                doc = doc.next;
+        if (PrintDocs){
+            for (String s : res){
+                System.out.println(s);
             }
-            return false;
-        }
-
-        public void AddDoc(WikiDoc doc) {
-            doc.next = docsIn;
-            docsIn = doc;
         }
     }
+    public static void PreProcessAndSearchIndex2(String filepath, String str) {
+        Index2 i2 = new Index2(filepath);
+        SearchIndex2(str, i2);
+    }
 
-    public Index1(String filename) {
-        String word;
-        WikiItem current, tmp;
-        try {
-            Scanner input = new Scanner(new File(filename), "UTF-8");
-            word = input.next();
-            start = new WikiItem(word, null);
-            current = start;
-            String doc_name = word;
-            WikiDoc doc = new WikiDoc(doc_name, null);
-            boolean take_next = false;
-            while (input.hasNext()) {   // Read all words in input
+    public static void SearchIndex3(String str, Index3 index3) {
+        var res = index3.search(str);
+        System.out.println("Index3 Result: " + res);
 
-                take_next = word.equals(END);
-                word = input.next();
-                if (take_next){
-                    doc_name = word;
-                    doc = new WikiDoc(doc_name, null);
-                }
-
-                System.out.println(word);
-                tmp = GetWikiItem(word);
-                if (GetWikiItem(word) == null){
-                    tmp = new WikiItem(word, null);
-                    current.next = tmp;
-                    current = tmp;
-                }
-                tmp.AddDoc(doc);
-
+        if (PrintDocs){
+            while (res != null){
+                System.out.println(res.header);
+                res = res.next;
             }
-            input.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("Error reading file " + filename);
         }
     }
-
-    public WikiItem GetWikiItem(String word){
-        WikiItem item = start;
-        while (item != null){
-            if (item.str.equals(word)) return item;
-
-            item = item.next;
-        }
-        return item;
+    public static void PreProcessAndSearchIndex3(String filepath, String str) {
+        Index3 i3 = new Index3(filepath);
+        SearchIndex3(str, i3);
     }
 
-    public WikiDoc search(String searchstr) {
-        WikiItem current = start;
-        String doc_header = start.str;
-        WikiItem headers = null;
-        while (current != null) {
-            if (current.str.equals(searchstr)) {
-                return current.docsIn;
-            }
-            current = current.next;
-        }
-        return null;
+    public static void SearchIndex4(String str, Index4 index4) {
+        var res = index4.search(str);
+        System.out.println("Index4 Result: " + res);
     }
+    public static void PreProcessAndSearchIndex4(String filepath, String str) {
+        Index4 i4 = new Index4(filepath);
+        SearchIndex4(str, i4);
+    }
+
 
     public static void main(String[] args) {
-        System.out.println("Preprocessing " + args[0]);
-        Index1 i = new Index1(args[0]);
         Scanner console = new Scanner(System.in);
+        String file_name = args[0];
         for (;;) {
             System.out.println("Input search string or type exit to stop");
             String searchstr = console.nextLine();
             if (searchstr.equals("exit")) {
                 break;
             }
-            WikiDoc res = i.search(searchstr);
-            if (res != null) {
-                System.out.println(searchstr + " exists");
-                while (res != null){
-                    System.out.println(res.header);
-                    res = res.next;
-                }
-            } else {
-                System.out.println(searchstr + " does not exist");
-            }
+            long startTime = System.nanoTime();
+            PreProcessAndSearchIndex1(file_name, searchstr);
+            long endTime = System.nanoTime();
+            long duration1 = (endTime - startTime)/1000000; // i milis
+
+            startTime = System.nanoTime();
+            PreProcessAndSearchIndex2(file_name, searchstr);
+            endTime = System.nanoTime();
+            long duration2 = (endTime - startTime)/1000000; // i milis
+
+            startTime = System.nanoTime();
+            PreProcessAndSearchIndex3(file_name, searchstr);
+            endTime = System.nanoTime();
+            long duration3 = (endTime - startTime)/1000000; // i milis
+
+            System.out.println("Index 1 time: " + duration1);
+            System.out.println("Index 2 time: " + duration2);
+            System.out.println("Index 3 time: " + duration3);
+
         }
         console.close();
     }
