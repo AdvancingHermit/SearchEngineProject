e06f87c0feac12ca1ca28683d944cf6ce91d869b
Oscar Stilling <s235281@dtu.dk>
2026-01-23 16:47:06 +0100

    fixes

--- src/core/interfaces.h
+++ src/core/interfaces.h
@@ -1,3 +1,4 @@
+#include <cstdint>
 #include <string>
 #include <vector>
 
@@ -8,35 +9,35 @@ public:
 };
 class ScoredDoc : public Doc {
 public:
-    int score;
+    int score{};
 };
 
 class IStore {
 public:
     virtual ~IStore() = default;
-    virtual add(Doc);
-    virtual Doc get(string title);
+    virtual void add(Doc);
+    virtual Doc get(std::string title);
 };
 class IPreprocessor {
 public:
     virtual ~IPreprocessor() = default;
-    virtual void* preprocess(string filename, IStore*);
+    virtual void* preprocess(std::string filename, IStore*);
 };
 
 class IHash {
 public:
     virtual ~IHash() = default;
-    virtual uint64_t hash(const std::string &key);
+    virtual std::uint64_t hash(const std::string &key);
 };
 
 class ISearcher {
 public:
     virtual ~ISearcher() = default;
-    virtual std::vector<Doc> search(string * q, IStore*) = 0;
+    virtual std::vector<Doc> search(std::string * q, IStore*);
 };
 
 class IRanker {
 public:
     virtual ~IRanker() = default;
-    virtual std::vector<ScoredDoc> rank(const std::vector<Doc>* candidates, string query) = 0;
+    virtual std::vector<ScoredDoc> rank(const std::vector<Doc>* candidates, std::string query);
 };
